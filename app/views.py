c3b756c7cd881c30db513ce41884dc8692ff261c
ruh-iziki <satyamp1947@gmail.com>
2021-05-18 00:53:33 +0530

    fourth commit

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlaced
 from .forms import CustomerRegistrationForm,CustomerProfileForm
@@ -32,13 +32,25 @@ class ProductDetailView(View):
 
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+ user=request.user
+ product_id = request.GET.get('prod_id')
+ product =Product.objects.get(id=product_id)
+ Cart(user=user,product=product).save()
+ return redirect('/cart')
+
+
+def show_cart(request):
+    if request.user.is_autheticated:
+        user=request.user
+        cart=Cart.objects.filter(user=user)
+        return render(request, 'app/addtocart.html',{'carts':cart})
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
 
 def address(request):
- return render(request, 'app/address.html')
+ add=Customer.objects.filter(user=request.user)
+ return render(request, 'app/address.html',{'add':add,'active':'btn-primary'})
 
 def orders(request):
  return render(request, 'app/orders.html')
