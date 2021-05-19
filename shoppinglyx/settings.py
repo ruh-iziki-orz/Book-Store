81ef98250da4d33bed35ff18bc4ff8273c86a201
ruh-iziki <satyamp1947@gmail.com>
2021-05-20 00:43:58 +0530

    last

--- shoppinglyx/settings.py
+++ shoppinglyx/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / 'media/productimg'
 LOGIN_REDIRECT_URL = '/profile'
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
